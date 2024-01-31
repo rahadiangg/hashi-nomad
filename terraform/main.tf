55b59992956a99fc9df2a96c800282a91b8d9702
rahadiangg <rahadian.arif@ainosi.co.id>
2024-01-31 21:51:47 +0700

    add alias provider aws nomad_clinet_jakarta

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   access_key = var.aws_ak
   secret_key = var.aws_sk
   region     = local.nomad_client_region
+  alias = "nomad_client_jakarta"
 }
 
 locals {
@@ -12,6 +13,9 @@ module "nomad_client" {
   source        = "./_network"
   resource_name = "nomad_client"
   region        = local.nomad_client_region
+  providers = {
+    aws = aws.nomad_client_jakarta
+  }
 }
 
 module "nomad_client_ec2" {
@@ -19,4 +23,7 @@ module "nomad_client_ec2" {
   instance_count = 2
   subnets        = module.nomad_client.public_subnet_ids
   instance_type  = "t3.micro"
+  providers = {
+    aws = aws.nomad_client_jakarta
+  }
 }
